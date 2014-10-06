6796b34178c6887d266a08f9a5b68d8fa89ce8a8
piper4123w <kyle.lawson@marquette.edu>
2014-10-05 23:36:35 -0500

    Changed a name

---
+++ src/AppleQueue.java
@@ -0,0 +1,38 @@
+
+public class AppleQueue implements Queue {
+
+  public AppleQueue() {
+    // TODO Auto-generated constructor stub
+  }
+
+  @Override public boolean enqueue(Object item) {
+    // TODO Auto-generated method stub
+    return false;
+  }
+
+  @Override public Object dequeue() {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override public int size() {
+    // TODO Auto-generated method stub
+    return 0;
+  }
+
+  @Override public void doubleQueue() {
+    // TODO Auto-generated method stub
+    
+  }
+
+  @Override public boolean isFull() {
+    // TODO Auto-generated method stub
+    return false;
+  }
+
+  @Override public boolean isEmpty() {
+    // TODO Auto-generated method stub
+    return false;
+  }
+
+}
