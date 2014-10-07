62677a18b6427e3fda65e4b4fb51a321bd9611d9
piper4123w <kyle.lawson@marquette.edu>
2014-10-06 19:55:35 -0500

    commented

--- src/AppleQueue.java
+++ src/AppleQueue.java
@@ -19,35 +19,35 @@ public class AppleQueue implements Queue {
   }
 
   @Override public Object dequeue() {
-    Customer tempCustomer = line[0];
-    for (int i = 0; i <= end; i++) {
+    Customer tempCustomer = line[0];    //gets the customer at the front of the line
+    for (int i = 0; i <= end; i++) {    //everyone moves up one spot
       line[i] = line[i + 1];
     }
-    return tempCustomer;
+    return tempCustomer;    //returns the front customer
   }
 
   @Override public int size() {
-    return end + 1;
+    return end + 1;   //returns the number of people in line
   }
 
   @Override public void doubleQueue() {
     capacity = capacity * 2;
-    Customer[] newQueArray = new Customer[capacity];
-    for (int i = 0; i < line.length; i++) {
+    Customer[] newQueArray = new Customer[capacity];    //makes a new line with twice the cap
+    for (int i = 0; i < line.length; i++) {   //moves all customers into the new line
       newQueArray[i] = line[i];
     }
-    line = newQueArray;
+    line = newQueArray;     //makes the new line the name of the old line so we can do this again later
   }
 
   @Override public boolean isFull() {
-    if (end == line.length - 1) {
+    if (end == capacity) {   //if the end spot reference equals the capacity
       return true;
     }
     return false;
   }
 
   @Override public boolean isEmpty() {
-    if(line[0] == null) {
+    if(line[0] == null) {   //if the first spot in line is empty
       return true;
     }
     return false;
