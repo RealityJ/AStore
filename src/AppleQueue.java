59b54d17bd5c08ec8b7edc63d64904d7bfaeef6f
piper4123w <kyle.lawson@marquette.edu>
2014-10-06 19:52:01 -0500

    here is the queue

--- src/AppleQueue.java
+++ src/AppleQueue.java
@@ -1,59 +1,53 @@
 public class AppleQueue implements Queue {
 
   private int        capacity;
-  private Customer[] queArray;
-  private int        front;
-  private int        end;
+  private Customer[] line;
+  private int        end = -1;
 
   public AppleQueue(int qCap) {
-    capacity = qCap;
-    queArray = new Customer[capacity];
-    front = -1;
-    end = 0;
+    line = new Customer[qCap];
   }
 
-  @Override public boolean  enqueue(Object customer) {
-    if(end != capacity) {
-      queArray[front] = (Customer) customer;
+  @Override public boolean enqueue(Object customer) {
+    if ((line[line.length - 1]) == null) {   // if the last slot of the line is an open slot will be possible
+      end++;          // new end spot in line because of added customer
+      line[end] = (Customer) customer;    // adds customer to end of the line
+      return true;
     }
+    System.out.println("line was full");
     return false;
   }
 
   @Override public Object dequeue() {
-    Customer firstCust = queArray[front++];
-    if(front == capacity) {
-      front = 0;
+    Customer tempCustomer = line[0];
+    for (int i = 0; i <= end; i++) {
+      line[i] = line[i + 1];
     }
-    return firstCust;
+    return tempCustomer;
   }
 
   @Override public int size() {
-    if (end >= front) {
-      return end - front;
-    }
-    else {
-      return (capacity - front) + (end + 1);
-    }
+    return end + 1;
   }
 
   @Override public void doubleQueue() {
     capacity = capacity * 2;
     Customer[] newQueArray = new Customer[capacity];
-    for(int i = 0; i < queArray.length; i++) {
-      newQueArray[i] = queArray[i];
+    for (int i = 0; i < line.length; i++) {
+      newQueArray[i] = line[i];
     }
-    queArray = newQueArray;
+    line = newQueArray;
   }
 
   @Override public boolean isFull() {
-    if (front + capacity - 2 == end || end + 2 == front) {
+    if (end == line.length - 1) {
       return true;
     }
     return false;
   }
 
   @Override public boolean isEmpty() {
-    if (end + 1 == front || (front + capacity - 1 == end)) {
+    if(line[0] == null) {
       return true;
     }
     return false;
