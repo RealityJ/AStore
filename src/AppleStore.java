701258a3845cd748bfaa0e67608e9b520d24a238
piper4123w <kyle.lawson@marquette.edu>
2014-10-07 16:40:26 -0500

    fixxed all of the sorting and handling of the customers by adding arraylists

--- src/AppleStore.java
+++ src/AppleStore.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 class AppleStore {
   final int  _minToHourConversion = 60;
   int        _capacity;
@@ -32,34 +34,37 @@ class AppleStore {
 
   public void simulation() {
     customerArrivalSimulation();
+
   }
 
   public void customerArrivalSimulation() {
-    System.out.println("_sim=" + _sim);
     for (int i = 0; i < _sim; i++) {    // runs simulation for desired hours
       System.out.println(i + ":hour");
-      unsorted = new Customer[_cPerHour];    // creates a temp unsorted Customer array
-      unsortedImpatient = new Customer[_cPerHour];
+      ArrayList<Customer> unsorted = new ArrayList<Customer>();    // creates a temp unsorted Customer array
+      ArrayList<Customer> unsortedImpatient = new ArrayList<Customer>();
       int impatientCount = 0;
+      int patientCount = 0;
       for (int j = 0; j < _cPerHour; j++) {   // creates Customers for the number that arrive per hour
-        if (_cPerHour > 5) {
-          System.out.println("more then 5 people per hour");
+        if (j >= 5) {
+//          System.out.println("more then 5 people per hour");
           if (Math.random() >= 0.5) {   // ~50% chance of turning away
-            unsorted[j - impatientCount] = new Customer((int) (Math.random() * 61)); // random int 0-60min
-            System.out.println(unsorted[j - impatientCount].getArrivalTime());
+            unsorted.add(new Customer((int) (Math.random() * 61))); // random int 0-60min
+            System.out.println(unsorted.get(patientCount).getArrivalTime() + " was patient");
+            patientCount++;
           }
           else {
-            unsortedImpatient[impatientCount] = new Customer((int) (Math.random() * 61));
+            unsortedImpatient.add(new Customer((int) (Math.random() * 61)));
+            System.out.println(unsortedImpatient.get(impatientCount).getArrivalTime() + " was NOT patient");
             impatientCount++;
-            System.out.println(unsortedImpatient[impatientCount].getArrivalTime());
           }
         }
         else {
-          unsorted[j] = new Customer((int) (Math.random() * 61));   // random int 0-60min
-          System.out.println(unsorted[j].getArrivalTime());
+          unsorted.add(new Customer((int) (Math.random() * 61)));   // random int 0-60min
+          System.out.println(unsorted.get(patientCount).getArrivalTime());
+          patientCount++;
         }
       }
-      if (unsortedImpatient[0] != null) {
+      if (!unsortedImpatient.isEmpty()) {
         sortImpatients(unsortedImpatient);
       }
       sort(unsorted);   // sorts the unsorted customer array by arrival and enters them into queue
@@ -72,46 +77,47 @@ class AppleStore {
    * @return
    */
 
-  public void sort(Customer[] unsorted) {
+  public void sort(ArrayList<Customer> unsorted2) {
     System.out.println("sorting the patient customers");
-    Customer[] sorted = new Customer[unsorted.length];
+    Customer[] sorted = new Customer[unsorted2.size()];
     int earliestPos = 0;
-    for(int j = 0; j < unsorted.length; j++) {
+    for (int j = 0; j < unsorted2.size(); j++) {
       Customer earliest = null;
-      for(int i = 0; i < unsorted.length; i++) {
-        if(unsorted[i] != null && (earliest == null || earliest.getArrivalTime() > unsorted[i].getArrivalTime())) {
-          earliest = unsorted[i];
+      for (int i = 0; i < unsorted2.size(); i++) {
+        if (unsorted2.get(i) != null && (earliest == null || earliest.getArrivalTime() > unsorted2.get(i).getArrivalTime())) {
+          earliest = unsorted2.get(i);
           earliestPos = i;
         }
       }
       sorted[j] = earliest;
-      unsorted[earliestPos] = null;
+      unsorted2.set(earliestPos,null);
       System.out.println(sorted[j].getArrivalTime());
     }
-    for(int i = 0; i < sorted.length; i++) {
+    for (int i = 0; i < sorted.length; i++) {
       queue.enqueue(sorted[i]);
       System.out.println("pushed customer at " + sorted[i].getArrivalTime() + " to the queue");
     }
   }
 
-  public void sortImpatients(Customer[] unsorted) {
-    System.out.println("sorting the patient customers");
-    Customer[] sorted = new Customer[unsorted.length];
+  public void sortImpatients(ArrayList<Customer> unsortedImpatient2) {
+    System.out.println("sorting the IMPATIENT customers");
+    Customer[] sorted = new Customer[unsortedImpatient2.size()];
     int earliestPos = 0;
-    for(int j = 0; j < unsorted.length; j++) {
+    for (int j = 0; j < unsortedImpatient2.size(); j++) {
       Customer earliest = null;
-      for(int i = 0; i < unsorted.length; i++) {
-        if(unsorted[i] != null && (earliest == null || earliest.getArrivalTime() > unsorted[i].getArrivalTime())) {
-          earliest = unsorted[i];
+      for (int i = 0; i < unsortedImpatient2.size(); i++) {
+        if (unsortedImpatient2.get(i) != null && (earliest == null || earliest.getArrivalTime() > unsortedImpatient2.get(i).getArrivalTime())) {
+          earliest = unsortedImpatient2.get(i);
           earliestPos = i;
         }
       }
       sorted[j] = earliest;
-      unsorted[earliestPos] = null;
+      unsortedImpatient2.set(earliestPos,null);
       System.out.println(sorted[j].getArrivalTime());
     }
-    for(int i = 0; i < sorted.length; i++) {
-      impatientQueue.enqueue(sorted[i]);
+    for (int i = 0; i < sorted.length; i++) {
+      queue.enqueue(sorted[i]);
+      System.out.println("pushed customer at " + sorted[i].getArrivalTime() + " to the IMPATIENT queue");
     }
   }
 
