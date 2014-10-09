b4c8ff75776d5b0159846ff29830e77fce4f944c
piper4123w <kyle.lawson@marquette.edu>
2014-10-09 00:35:38 -0500

    initial Commit

---
+++ src/TopTenPlayersList.java
@@ -0,0 +1,25 @@
+
+public class TopTenPlayersList {
+  private LLNode<Player> list;
+
+  public TopTenPlayersList() {
+    list = new LLNode<Player>();
+    // TODO Auto-generated constructor stub
+  }
+
+  public void addEntry(String name, double score) {
+    
+  }
+  
+  public void removeFirst() {
+    
+  }
+  
+  public void removeLast() {
+    
+  }
+  
+  public String toString() {
+    
+  }
+}
